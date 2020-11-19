4af225f1046f0f0822dd87042172843b130940c8
John Million <jpmillion@comcast.net>
2020-11-19 19:13:18 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
-  
-  def reset_all
-    all.clear
-  end
-  
-  def count
-    all.count
-  end
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+    
+    def count
+      all.count
+    end
   
 end
